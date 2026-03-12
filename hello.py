570a20819773614b7b9772154f6697e63cd0d923
kellychen-ui <chen233436@gmail.com>
2026-03-12 21:10:25 +0800

    python work

--- hello.py
+++ hello.py
@@ -1,14 +1,12 @@
 def square(y):
-	print(f"{y}的三次方式{y**3}")
+	print(f"{y * (y+1) // 2}")
 
 
 
 x = int(input("請輸入一個正整數："))
 #x += 10:
 
-if (x<=0):
-	print(f"您輸入的值是{x},小於等於0")
-else:
-	print(f"您輸入的值是{x},大於0")
-for i in range(1, x+1):
+for i in range(1,x+1):
 	#print(i, end=";")
+	square(i)
+
